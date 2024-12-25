0dac765fc2b625d07959ef94c184995bc4b9e7c0
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-12-25 16:50:41 +0100

    fixed neotree and added terminal  setings

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '\\', ':Neotree reveal position=current <CR>', desc = 'NeoTree reveal' },
   },
   opts = {
     filesystem = {
