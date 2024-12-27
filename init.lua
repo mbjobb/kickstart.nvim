b0ef34e8a2db8cbe4f9c685f0ac8801bb6b7f863
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-12-27 14:57:54 +0100

    set termguicolors

--- init.lua
+++ init.lua
@@ -810,6 +810,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.opt.termguicolors = true
       vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
