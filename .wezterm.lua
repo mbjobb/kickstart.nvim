664d8b4ea1f36c865a917e8d589c199ada163fa2
mbJobb <71829138+mbJobb@users.noreply.github.com>
2025-01-07 08:18:56 +0100

    added padding for surface laptop and diferentiated between it and desktop

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,12 +12,19 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 4,
-  bottom = 0,
-}
+-- config.window_padding = { --desktop
+--   left = 0,
+--   right = 0,
+--   top = 4,
+--   bottom = 0,
+-- }
+--
+-- config.window_padding = { --surface
+-- 	left = 3,
+-- 	right = 0,
+-- 	top = 21,
+-- 	bottom = 100,
+-- }
 
 -- and finally, return the configuration to wezterm
 return config
